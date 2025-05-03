f4a5ea7a4ef1d188a5b21c1e135c84a6d4c1685c
MytriAryani <mytriaryanidkmr@gmail.com>
2025-05-03 21:15:41 +0530

    added basic functionality

---
+++ script.js
@@ -0,0 +1,35 @@
+
+const shareButton=document.querySelector("#share-button");
+shareButton.addEventListener("click", async()=>{
+ const name= document.querySelector("#name").value.trim();
+ const phone= document.querySelector("#phone").value.trim();
+ const status= document.querySelector("#status");
+ const data={
+    name,
+    phone
+ }
+
+ const jsonString=JSON.stringify(data,null,2);
+  
+ const file = new Blob([jsonString],{type:"application/json"});
+
+ const jsonFile= new File([file],"data.json",{
+    type:"application/json" });
+
+
+if (navigator.canShare && navigator.canShare({ files: [jsonFile] })) {
+    try {
+        await navigator.share({
+        title: "User Data",
+        text: "Here is my user data JSON file.",
+        files: [jsonFile]
+        });
+        status.textContent="File shared successfully!";
+    } catch (error) {
+        console.error("Sharing failed:", error);
+        status.textContent="Error sharing file.";
+    }
+    } else {
+    status.textContent="Sharing not supported on this device.";
+    }
+});
